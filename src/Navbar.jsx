a5c0355c46969b5f80d74b182c803318e2e5731d
Lantao-bit <lantao1965@gmail.com>
2026-02-09 07:58:54 +0000

    Refactor navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,35 +1,43 @@
-import React, { useState } from 'react'
-export default frunction Navbar(){
+import React, {useState} from 'react'
+import { Link } from 'wouter';
+
+export default function Navbar() {
+
     const [isNavbarShowing, setNavbarShowing] = useState(false);
+
+    const toggleNavbar = () => {
+        setNavbarShowing(!isNavbarShowing);
+    }
+
     return (<>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">E-Shop</a>
+                <Link className="navbar-brand" href="/">E-Shop</Link>
                 <button
                     className="navbar-toggler"
                     type="button"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    onClick={toggleNavbar}
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isNavbarShowing ? "show" : ""}`} id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" href="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Products</a>
+                            <Link className="nav-link" href="/products">Products</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
+                            <Link className="nav-link" href="/register">Register</Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
+
     </>)
-
 }
-
